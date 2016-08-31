94a2d5789a48e3348b03de43991313dde797b3d0
Marko Matic <mail.marko.matic@gmail.com>
2016-08-31 16:17:02 +0200

    Sample for Xamarin Bug 43751

---
+++ XamarinIssue/SomeViewController.cs
@@ -0,0 +1,26 @@
+
+using UIKit;
+
+namespace XmarinIssue
+{
+    public partial class SomeViewController : UIViewController
+    {
+        public SomeViewController() : base("SomeViewController", null)
+        {
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+
+            // Release any cached data, images, etc that aren't in use.
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            // Perform any additional setup after loading the view, typically from a nib.
+        }
+    }
+}
